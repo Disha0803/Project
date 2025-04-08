0e0a69800cef87a3903e0967c7b2ce908a1e20d4
M Disha <manvidisha080@gmail.com>
2025-04-08 14:29:42 +0530

    done

--- heart_disease_prediction/heart_disease_prediction/settings.py
+++ heart_disease_prediction/heart_disease_prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-dzapxk-zc@izlthxtk*3bh_zbf28w&zm_gqi$sx@nssctit=u(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['prediction.onrender.com']
 
 
 # Application definition
