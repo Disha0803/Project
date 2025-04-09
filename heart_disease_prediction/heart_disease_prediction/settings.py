47bd0db80babf4db8c7fdd0f96559feb6d68ae7e
M Disha <manvidisha080@gmail.com>
2025-04-10 00:59:44 +0530

    final

--- heart_disease_prediction/heart_disease_prediction/settings.py
+++ heart_disease_prediction/heart_disease_prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-dzapxk-zc@izlthxtk*3bh_zbf28w&zm_gqi$sx@nssctit=u(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dear-heart-tab4.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['dearheart-tab4.onrender.com', 'localhost', '127.0.0.1']
 
 
 
